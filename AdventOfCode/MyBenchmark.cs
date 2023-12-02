4448de096e2c036a8cca195c946e4457c9c49624
Mihai Luca <luca.mihai97@yahoo.com>
2023-12-02 15:29:14 +0200

    Add benchmarking

---
+++ AdventOfCode/MyBenchmark.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace AdventOfCode;
+
+[MemoryDiagnoser]
+[RankColumn]
+public class MyBenchmark
+{
+    [Benchmark]
+    public void Test()
+    {
+        Year2023.Day02.Solution.Solve();
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,19 +1,15 @@
-﻿namespace AdventOfCode;
+﻿using BenchmarkDotNet.Running;
+
+namespace AdventOfCode;
 
 public static class Program
 {
     public static void Main(string[] args)
     {
-        //Year2022.Day01.Solution.Solve();
-        //Year2022.Day02.Solution.Solve();
-        //Year2022.Day03.Solution.Solve();
-        //Year2022.Day04.Solution.Solve();
-        //Year2022.Day05.Solution.Solve();
-        //Year2022.Day06.Solution.Solve();
-        //Year2022.Day07.Solution.Solve();
-
-        //Year2023.Day01.Solution.Solve();
         Year2023.Day02.Solution.Solve();
+
+        // Uncomment below line for benchmarking
+        //BenchmarkRunner.Run<MyBenchmark>();
 
         Console.Read();
     }
